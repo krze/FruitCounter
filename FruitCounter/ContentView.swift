eb020161b732696a31fb1f704cf74f0d130cfb9d
krze <kenkrzeminski@gmail.com>
2020-07-03 10:25:30 -0400

    Initial setup

---
+++ FruitCounter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FruitCounter
+//
+//  Created by Ken Krzeminski on 7/3/20.
+//  Copyright © 2020 Ken Krzeminski. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
